370ed8d3b8c039ad6601a9d740aca4a1a806a5af
sliupp <simonliu123456789@gmail.com>
2022-06-24 21:19:04 -0700

    added to Specs and Student.java

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,45 @@
 public class Student {
     private String name;
     private int id;
-    private int fees;
-    
+    private double balance;
+    private boolean status;
+
+    // Constructor
+    public Student (String name, int id, double balance) {
+        this.name = name;
+        this.id = id;
+        this.balance = balance;
+        status = true;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void payment(double amount) {
+        balance -= amount;
+    }
+
+    public void charge(double amount) {
+        balance += amount;
+    }
+
+    public void changeStatus() {
+        status = !status;
+    }
+
+
 }
